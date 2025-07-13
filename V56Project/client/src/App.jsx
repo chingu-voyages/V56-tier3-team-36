82458fcf9b15abd32a31606331b0abe4a0df1ad9
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-07-13 12:49:24 -0400

    add function for time and changed color of icon

---
+++ V56Project/client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom'
+import './App.css'
+
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
+
+
+export default App
---
+++ V56Project/client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header'
+function Home() {
+  return (
+    <div className='w-full h-full flex items-center justify-center'>
+ <Header />
+ </div>
+  )
+}
+
+export default Home
