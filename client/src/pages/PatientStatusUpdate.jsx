c72ed39e9d5ad2c4fa256b13815017461351cfda
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-07-15 17:07:10 -0400

    added styling to FindPatient component, added margins to StatusFlowGuide component  and FindPatient component, imported React Icons to add search icon and arrows to components

--- client/src/pages/PatientStatusUpdate.jsx
+++ client/src/pages/PatientStatusUpdate.jsx
@@ -3,15 +3,13 @@ import Header from '../components/Header';
 import StatusFlowGuide from '../components/StatusFlowGuide';
 import FindPatient from '../components/FindPatient';    
 import Footer from '../components/Footer';
-
 export default function PatientStatusUpdate() {
     return (
         <div>
         <Header />
         <div className="bg-blue-background pt-40">
         <h1 className='text-4xl font-bold'>Patient Status Update</h1>
-        <p className='text-xl text-gray-600 p-6 '>Update patient status throuout the surgical journey. Enter patient number to begin.</p>
-        
+        <p className='text-xl text-gray-600 p-6 '>Update patient status throughout the surgical journey. Enter patient number to begin.</p>
     <StatusFlowGuide /> 
     <FindPatient />
     <Footer />
