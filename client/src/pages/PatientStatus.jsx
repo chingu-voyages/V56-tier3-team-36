1f178af7ccde7e042c0911b6e32ed8dbf1c988c3
Angi Boiciuc <aboiciuc@catalyte.io>
2025-07-15 16:20:14 -0500

    Adding Patient Status Page

---
+++ client/src/pages/PatientStatus.jsx
@@ -0,0 +1,10 @@
+import Footer from "../components/Footer"
+
+export default function PatientStatus(){
+  return(
+    <>
+    <div className='w-full h-full flex items-center justify-center'>Patient Status Page - Work in progress</div>
+    <Footer /> 
+    </>
+  )
+}
