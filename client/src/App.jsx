f44387d0628b9a17e26ad6f13844e178edd81aa6
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-07-24 13:10:45 -0400

    Resolved merge conflict in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,8 @@ import './App.css'
 
 import Home from './pages/Home'
 import PatientInformation from './pages/PatientInformation'
-<<<<<<< HEAD
 import PatientStatusUpdate from './pages/PatientStatusUpdate'
-=======
 import PatientStatus from './pages/PatientStatus'
->>>>>>> 060247324b9599334b2cff7a1226ed6fa64823ab
 
 function App() {
   return (
@@ -16,11 +13,8 @@ function App() {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/patientinformation" element={<PatientInformation />} />
-<<<<<<< HEAD
         <Route path="/patientstatusupdate" element={<PatientStatusUpdate />} />
-=======
         <Route path="/patientstatus" element={<PatientStatus />} />
->>>>>>> 060247324b9599334b2cff7a1226ed6fa64823ab
       </Routes>
     </Router>
   )
